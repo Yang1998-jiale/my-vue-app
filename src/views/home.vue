fc1d74c80a1ed20dfd14e2bf7e9041e670df7432
yjl <2018770090@qq.com>
2024-04-17 15:58:05 +0800

    测试ci/cd

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,26 +2,22 @@
  * @Author: yjl
  * @Date: 2023-10-16 10:30:39
  * @LastEditors: yjl
- * @LastEditTime: 2024-04-15 16:41:01
+ * @LastEditTime: 2024-04-17 15:57:47
  * @Description: 描述
 -->
 <template>
   <div>
-    
     <div class="box">
       <BpmnCanvas bpmnID="test" />
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-import { reactive, onMounted } from "vue";
+import { onMounted } from "vue";
 import { BpmnCanvas } from "bpmn-vue-temp";
-let state = reactive<any>({});
 onMounted(() => {});
 </script>
 <style scoped>
-
-
 .box {
   width: 1000px;
   height: 1000px;
