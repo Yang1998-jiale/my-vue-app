170f3a3290bb0bf6e43203fab07b67fbd6c2c9fa
yjl <2018770090@qq.com>
2024-04-17 16:22:39 +0800

    测试

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,7 +2,7 @@
  * @Author: yjl
  * @Date: 2023-10-16 10:30:39
  * @LastEditors: yjl
- * @LastEditTime: 2024-04-17 16:20:17
+ * @LastEditTime: 2024-04-17 16:22:34
  * @Description: 描述
 -->
 <template>
@@ -14,8 +14,6 @@
 </template>
 <script lang="ts" setup>
 import { onMounted } from "vue";
-
-
 import { BpmnCanvas } from "bpmn-vue-temp";
 onMounted(() => {});
 </script>
